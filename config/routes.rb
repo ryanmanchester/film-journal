c64b8667792ef7d1ee10d9a0439dac16e0352e7b
ryanmanchester <ryanm17@gmail.com>
2019-01-23 13:46:24 -0500

    first test commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'static/welcome'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'static#welcome'
 end
