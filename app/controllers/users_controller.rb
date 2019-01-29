c8d99d7f3220b3d868a64b0f0c47e4e62c5d8d67
ryanmanchester <ryanm17@gmail.com>
2019-01-29 15:23:56 -0500

    added a working sessions controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,14 +7,15 @@ class UsersController < ApplicationController
   def create
     if !params[:user][:username].blank? && !params[:user][:password].blank?
       @user = User.create(user_params)
+      log_in(@user)
       redirect_to user_path(@user)
     else
-      redirect_to '/signin'
+      redirect_to '/signup'
+      flash[:alert] = "Username and email must be provided"
     end
   end
 
   def show
-
     @user = User.find_by(id: params[:id])
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  resources :users
   get 'static/welcome'
   get 'signup', to: 'users#new'
+  get 'signin', to: 'sessions#new'
+  post 'signin', to: 'sessions#create'
+  delete 'signout', to: 'sessions#destroy'
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static#welcome'
 end
