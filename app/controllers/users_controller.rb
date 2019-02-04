72d516b962aa8c16b276c48fe3ab8a14256dcaea
ryanmanchester <ryanm17@gmail.com>
2019-02-04 18:55:21 -0500

    added flash messages to browser

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       log_in(@user)
-      redirect_to user_path(@user), message: "Welcome back, #{@user.username}!"
+      redirect_to user_path(@user)
     else
       render 'new'
 
@@ -19,7 +19,8 @@ class UsersController < ApplicationController
     if logged_in?
       @user = User.find_by(id: params[:id])
   else
-    redirect_to root_path, message: "Please Sign In or Sign Up for an Account"
+    redirect_to root_path
+    flash[:message] = "Please Sign In or Sign Up for an Account"
   end
 end
 
