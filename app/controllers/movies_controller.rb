c78a908a3da1b29f1dfa0c0a6eae39567b73a7e3
ryanmanchester <ryanm17@gmail.com>
2019-02-09 15:05:55 -0500

    added a new movies form

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,14 @@
 class MoviesController < ApplicationController
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    byebug
+  end
+
+  private
+  def movie_params
+    params.require(:movie).permit(:title, :release_year, :director, :starring, :synopsis, :image)
+  end
 end
