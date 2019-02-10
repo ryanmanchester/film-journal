b03d033c5ded966af488921aacb32981b6520dee
ryanmanchester <ryanm17@gmail.com>
2019-02-09 19:31:38 -0500

    added custon reader and writers for directors of movies

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,12 @@ class Movie < ApplicationRecord
   belongs_to :director
   has_many :user_movies
   has_many :users, through: :user_movies
+
+  def director_name=(name)
+    self.director = Director.find_or_create_by(name: name)
+  end
+
+  def director_name
+    self.director ? self.director.name : nil
+  end
 end
---
+++ db/migrate/20190210002230_remove_director_from_movie.rb
@@ -0,0 +1,5 @@
+class RemoveDirectorFromMovie < ActiveRecord::Migration[5.2]
+  def change
+    remove_column :movies, :director, :string
+  end
+end
