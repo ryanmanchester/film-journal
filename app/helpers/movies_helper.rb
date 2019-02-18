019b49a5e1bac45ca52c8bd4e6fc33fa514a9855
ryanmanchester <ryanm17@gmail.com>
2019-02-18 15:07:50 -0500

    working movies show page wth html template

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,2 +1,6 @@
 module MoviesHelper
+  def add_movie(movie)
+    #@user = User.find_by(id: session[:user_id])
+    @user.movies << movie
+  end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,7 +2,7 @@ class Movie < ApplicationRecord
   belongs_to :director
   has_many :user_movies
   has_many :users, through: :user_movies
-  validates :title, :starring, :director_name, :synopsis, :release_year, :image, presence: true
+  #validates :title, :starring, :director_name, :synopsis, :release_year, :image, presence: true
 
   def director_name=(name)
     self.director = Director.find_or_create_by(name: name)
