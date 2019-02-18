cd5202b70fab2947f5c5c0cf068806108b4a70f8
ryanmanchester <ryanm17@gmail.com>
2019-02-18 15:07:50 -0500

    working movies show page wth html template

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,25 +1,40 @@
 class MoviesController < ApplicationController
+  before_action :require_login
+  before_action :find_user, only: [:create, :show]
+
   def new
     @movie = Movie.new
-    redirect_to root_path unless logged_in?
-    flash[:message] = "Sign In to Add a Movie"
   end
 
   def create
-    user = User.find_by(id: session[:user_id])
-    #byebug
     @movie = Movie.new(movie_params)
+    #byebug
     if @movie.save
-      user.movies << @movie
-      redirect_to user_path(user)
+      add_movie(@movie)
+      redirect_to user_path(@user)
     else
       render :new
-
     end
+  end
+
+  def show
+    @movie = Movie.find(params[:id])
   end
 
   private
   def movie_params
     params.require(:movie).permit(:title, :release_year, :director_name, :starring, :synopsis, :image)
   end
+
+  def require_login
+    unless logged_in?
+    flash[:message] = "Sign In to Add a Movie"
+    redirect_to signin_path
+  end
+end
+
+  def find_user
+    @user = User.find_by(id: session[:user_id])
+  end
+
 end
