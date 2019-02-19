ac73662e2d0c70cd3f80a299cba27d0df99cf7f4
ryanmanchester <ryanm17@gmail.com>
2019-02-18 19:27:20 -0500

    starting the nested form to add reviews and ratings

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,12 +1,14 @@
 class MoviesController < ApplicationController
   before_action :require_login
-  before_action :find_user, only: [:create, :show]
+  before_action :find_user, only: [:create, :show, :update]
+  before_action :find_movie, only: [:show, :edit, :update]
 
   def new
     @movie = Movie.new
   end
 
   def create
+    byebug
     @movie = Movie.new(movie_params)
     #byebug
     if @movie.save
@@ -18,13 +20,29 @@ class MoviesController < ApplicationController
   end
 
   def show
-    @movie = Movie.find(params[:id])
+
+  end
+
+  def edit
+
+  end
+
+  def update
+byebug
+    if params[:movie][:user_movies]
+      @movie.user_movies.update(movie_params[:user_movies])
+    else
+      @movie.update(movie_params)
+    end
+    redirect_to user_path(@user)
+
   end
 
   private
   def movie_params
-    params.require(:movie).permit(:title, :release_year, :director_name, :starring, :synopsis, :image)
+    params.require(:movie).permit(:title, :release_year, :director_name, :starring, :synopsis, :image, user_movies_attributes: [])
   end
+
 
   def require_login
     unless logged_in?
@@ -35,6 +53,10 @@ end
 
   def find_user
     @user = User.find_by(id: session[:user_id])
+  end
+
+  def find_movie
+    @movie = Movie.find(params[:id])
   end
 
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,6 +3,7 @@ class Movie < ApplicationRecord
   has_many :user_movies
   has_many :users, through: :user_movies
   #validates :title, :starring, :director_name, :synopsis, :release_year, :image, presence: true
+ accepts_nested_attributes_for :user_movies
 
   def director_name=(name)
     self.director = Director.find_or_create_by(name: name)
