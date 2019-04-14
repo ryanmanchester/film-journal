de37bab8c2bc3e5c4861f07aa6370ed8df5ba754
ryanmanchester <ryanm17@gmail.com>
2019-04-14 19:01:33 -0400

    integrated omniauth-facebook to log in

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_10_235226) do
+ActiveRecord::Schema.define(version: 2019_04_14_224703) do
 
   create_table "directors", force: :cascade do |t|
     t.string "name"
@@ -43,6 +43,8 @@ ActiveRecord::Schema.define(version: 2019_02_10_235226) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "provider"
+    t.string "uid"
   end
 
 end
