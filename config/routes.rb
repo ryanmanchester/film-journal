548016fc0ce199ad8cbfb3d16f9fa28dbad793dc
ryanmanchester <ryanm17@gmail.com>
2019-04-15 17:20:32 -0400

    styled facebook for users#new

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,5 @@ Rails.application.routes.draw do
     resources :movies
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'static#welcome'
+  root 'sessions#new'
 end
