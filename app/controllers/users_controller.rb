f6e67ad9816fac806bbe2ff6f2ff7a5366a8bd73
ryanmanchester <ryanm17@gmail.com>
2019-05-22 16:28:20 -0400

    added 3 new scope methods and incorporated them into the index view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       log_in(@user)
-      redirect_to user_movies_path
+      redirect_to user_movies_path(@user)
     else
       render 'new'
     end
