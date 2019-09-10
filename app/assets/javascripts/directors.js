9a80843c813dde079a2de9fb13fc137b41ac42fb
Ryan Manchester <ryanm17@gmail.com>
2019-09-10 19:10:52 -0400

    gave different JSON calls own functions

--- app/assets/javascripts/directors.js
+++ app/assets/javascripts/directors.js
@@ -1,41 +1,7 @@
 $(document).ready(() => {
-    $('a.directors').on('click', (e) => {
-        alert('Clicked Button');
-        e.preventDefault();
-        history.pushState(null, null, '/directors')
-        $.getJSON('/directors', (data) => {
-          $('#body-container').html('');
-          data.forEach((director) => {
-            let newDirector = new Director(director);
-            let directorHtml = newDirector.formatIndex();
-            $('#body-container').append(directorHtml);
-          });
-        });
-      });
-      $(document).on('click', '.show-director', function(e) {
-        e.preventDefault();
-        alert('Show Director');
-        let id = $(this).attr('data-id');
-
-        $.getJSON(`/directors/${id}`, (data) => {
-          $('#body-container').html('');
-          history.pushState(null, null, `directors/${id}`)
-          let newDirector = new Director(data);
-          let directorHtml = newDirector.formatShow();
-          $('#body-container').append(directorHtml);
-          data.movies.forEach((movie) => {
-            let movieHtml = `
-             <ul>
-               <li>${movie.title}</li>
-              </ul>
-            `
-            $('#body-container').append(movieHtml);
-
-          })
-
-        });
-      });
-    });
+  getDirectors();
+  getDirector();
+});
 
   function Director(director){
     this.id = director.id;
@@ -57,3 +23,44 @@ $(document).ready(() => {
     `
     return showHtml;
   }
+
+  function getDirectors(){
+    $('a.directors').on('click', (e) => {
+        alert('Clicked Button');
+        e.preventDefault();
+        history.pushState(null, null, '/directors')
+        $.getJSON('/directors', (data) => {
+          $('#body-container').html('');
+          data.forEach((director) => {
+            let newDirector = new Director(director);
+            let directorHtml = newDirector.formatIndex();
+            $('#body-container').append(directorHtml);
+          });
+        });
+      });
+    }
+
+  function getDirector() {
+    $(document).on('click', '.show-director', function(e) {
+      e.preventDefault();
+      alert('Show Director');
+    let id = $(this).attr('data-id');
+    $.getJSON(`/directors/${id}`, (data) => {
+      $('#body-container').html('');
+      history.pushState(null, null, `directors/${id}`)
+      let newDirector = new Director(data);
+      let directorHtml = newDirector.formatShow();
+      $('#body-container').append(directorHtml);
+      data.movies.forEach((movie) => {
+        let movieHtml = `
+         <ul>
+           <li>${movie.title}</li>
+          </ul>
+        `
+        $('#body-container').append(movieHtml);
+
+      });
+
+    });
+  });
+}
