fb758d2bee38bab86e1619a58cb5e6d094018de0
github username <github email address>
2019-09-15 20:49:46 -0400

    started JS form submission

--- app/assets/javascripts/movies.js
+++ app/assets/javascripts/movies.js
@@ -1,30 +1,28 @@
 $(document).ready( () => {
-  $('#new_movie').on('submit', (e) => {
+  $('form#new_movie').on('submit', (e) => {
     e.preventDefault();
-    alert('Submit');
-    $('#body-container').html('');
-    $('#body-container').html(formatNewMovie());
+    debugger;
   })
 })
 
-function formatNewMovie() {
+ Movie.prototype.formatNewMovie = function() {
   let newMovieHtml = `<div class="container">
-  <h1 class="my-4">Title</h1>
+  <h1 class="my-4">${this.title}</h1>
   <div class="row">
 
       <div class="col-md-8">
-        <img class="img-fluid" src="" alt="">
+        <img class="img-fluid" src="${this.image}" >
       </div>
 
       <div class="col-md-4">
         <h3 class="my-3">Synopsis:</h3>
-        <p></p>
+        <p>${this.synopsis}</p>
         <h3 class="my-3">Starring:</h3>
-        <p></p>
+        <p>${this.starring}</p>
         <h3 class="my-3">Release Year:</h3>
-        <p></p>
+        <p>${this.release_year}</p>
         <h3 class="my-3">Director:</h3>
-        <p></p>
+        <p>${this.director}</p>
 
       </div>
 
@@ -32,4 +30,14 @@ function formatNewMovie() {
       </div>
   `
   return newMovieHtml;
+}
+
+function Movie(movie) {
+  this.id = movie.id;
+  this.title = movie.title;
+  this.release_year = movie.release_year;
+  this.starring = movie.starring;
+  this.synopsis = movie.synopsis;
+  this.image = movie.image;
+  this.director = movie.director;
 }
--- app/serializers/movie_serializer.rb
+++ app/serializers/movie_serializer.rb
@@ -1,4 +1,5 @@
 class MovieSerializer < ActiveModel::Serializer
-  attributes :id, :title, :image, :synopsis
+  attributes :id, :title, :release_year, :starring, :synopsis, :image
   belongs_to :director
+  has_many :users
 end
