3020cf1b8fd62e86421870f5fcd3203fef64d9ab
Ryan Manchester <ryanm17@gmail.com>
2019-09-16 15:41:03 -0400

    successful JS form submission

--- app/serializers/movie_serializer.rb
+++ app/serializers/movie_serializer.rb
@@ -1,5 +1,5 @@
 class MovieSerializer < ActiveModel::Serializer
   attributes :id, :title, :release_year, :starring, :synopsis, :image
   belongs_to :director
-  has_many :users
+  has_many :user_movies
 end
