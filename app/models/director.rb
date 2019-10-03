8414cfc4f0481bd52692525e34e00405e5a576ba
github username <github email address>
2019-10-03 19:42:27 -0400

    added a sort directors button for the live code assessment

--- app/models/director.rb
+++ app/models/director.rb
@@ -3,7 +3,7 @@ class Director < ApplicationRecord
 
   scope :popular_director, -> { self.joins(:movies).group(:id).order("count(movies.id) desc").limit(1)}
 
-  def sort_directors_name(directors)
-    directors.sort_by {|director| director.name}
-  end
+  # def sort_directors_name(directors)
+  #   directors.sort_by {|director| director.name}
+  # end
 end
